792a982dc4624ec8076c4f76838202cd7b48e754
Chakitha1 <chakkipalavelli@gmail.com>
2023-09-13 08:47:23 +0530

    adding table15

--- src/Day1/Table15.java
+++ src/Day1/Table15.java
@@ -1,12 +1,9 @@
-package Day1;
-
 public class Table15 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for (int i=10; i>=1; i--) {
-			System.out.println(i);
-		}
-	}
-
+    public static void main(String[] args) {
+        int number = 15;
+        for (int i = 1; i <= 10; i++) {
+            int result = number * i;
+            System.out.printf("%d * %d = %d%n", number, i, result);
+        }
+    }
 }
